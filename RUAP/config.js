3c15f4988fa13e15f8aabca9d7b7388f74564876
emanuelsavianu <emanuelsavianu@gmail.com>
2026-07-09 19:30:29 +0200

    agosto ruap

--- RUAP/config.js
+++ RUAP/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // Bump this when you change doctors, assignments, or other config data
   // so existing users get prompted to update their localStorage
-  configDataVersion: '2026-07-07b',
+  configDataVersion: '2026-07-09',
 
   places: ['M.S.Savino', 'Subbiano'],
 
@@ -159,7 +159,7 @@ const CONFIG = {
     "2026-08-05_pom_Subbiano": "mrasudwdk87n",
     "2026-08-06_mat_M.S.Savino": "mqgp5xpeohiai",
     "2026-08-06_mat_Subbiano": "mqgp5xpe6gb6b",
-    "2026-08-06_pom_M.S.Savino": "mqgp5xpekiw12",
+    "2026-08-06_pom_M.S.Savino": "mqgp5xpec24f6",
     "2026-08-06_pom_Subbiano": "mqgp5xpehbrke",
     "2026-08-07_mat_M.S.Savino": "mqgp5xpe8eeqy",
     "2026-08-07_mat_Subbiano": "mqgp5xpec24f6",
@@ -212,7 +212,7 @@ const CONFIG = {
     "2026-08-26_mat_Subbiano": "mqgp5xpec24f6",
     "2026-08-26_pom_M.S.Savino": "mqgp5xpeohiai",
     "2026-08-26_pom_Subbiano": "mrasudwdk87n",
-    "2026-08-27_mat_M.S.Savino": "mrasudwdk87n",
+    "2026-08-27_mat_M.S.Savino": "mqgp5xpeohiai",
     "2026-08-27_mat_Subbiano": "mqgp5xpe6gb6b",
     "2026-08-27_pom_M.S.Savino": "mqgp5xpe79zho",
     "2026-08-27_pom_Subbiano": "mqgp5xpehbrke",
@@ -224,6 +224,6 @@ const CONFIG = {
     "2026-08-31_mat_Subbiano": "mqgp5xpe6gb6b",
     "2026-08-31_pom_M.S.Savino": "mqgp5xpekiw12",
     "2026-08-31_pom_Subbiano": "mqgp5xpemfnc0",
-    "2026-08-13_pom_M.S.Savino": "mqgp5xpekiw12"
+    "2026-08-13_pom_M.S.Savino": "mqgp5xpec24f6"
   },
 };
